f6fc055754b0ba3783cad933e0bc8783b35707cd
Arsen <68658606+Arsen-go@users.noreply.github.com>
2022-03-04 19:08:16 +0400

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,3 @@
-const { authenticated, roleAuthentication, createToken, tradeTokenForUser } = require("./authentication");
+const { createToken, tradeTokenForUser } = require("./authentication");
 
-module.exports = { authenticated, tradeTokenForUser, roleAuthentication, createToken };
+module.exports = { tradeTokenForUser, createToken };
