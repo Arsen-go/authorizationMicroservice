d26cb701a986f90159f209c933d2df296e4c85b4
Arsen <68658606+Arsen-go@users.noreply.github.com>
2022-03-07 11:05:47 +0400

    FIX files

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,30 @@
-const { createToken, tradeTokenForUser } = require("./authentication");
+const jsonwebtoken = require("jsonwebtoken");
 
-module.exports = { tradeTokenForUser, createToken };
+class AuthController {
+  createToken(data, secret, expiresIn) {
+    const authToken = jsonwebtoken.sign(
+      data,
+      secret,
+      { expiresIn }
+    );
+
+    return authToken;
+  };
+
+  tradeTokenForUser(token, secret) {
+    try {
+      if (token && secret) {
+        token = token.replace("Bearer ", "");
+        const decoded = jsonwebtoken.verify(token, secret);
+
+        return { decoded };
+      } else {
+        return { decoded: null };
+      }
+    } catch (error) {
+      return { error };
+    }
+  };
+}
+
+module.exports = new AuthController();
